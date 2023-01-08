a8c515a1542fefea4edf6c0ba87a12fab2e6bd03
Phillip Rosenbaum <Phillip@PhillipRosenbaum.com>
2023-01-08 20:24:37 +0100

    added new methos to take the current data and add each row to a list

---
+++ BLL/ImportTeamData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Football_World_Cup_Score_Board_Test.BLL
+{
+   
+
+    public  class ImportTeamData
+    {
+
+        private readonly ILogger<ImportTeamData> _logger;
+        public ImportTeamData(ILogger<ImportTeamData> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<string> currentDataList = new List<string>();
+        public List<string> getTeamDataList()
+        {
+            _logger.LogInformation("getTeamDataList method called");
+            //current data in the system
+            try
+            {
+                currentDataList.Add("a.Mexico - Canada: 0 - 5");
+                currentDataList.Add("b.Spain - Brazil: 10 – 2");
+                currentDataList.Add("c.Germany - France: 2 – 2");
+                currentDataList.Add("d.Uruguay - Italy: 6 – 6");
+                currentDataList.Add("e.Argentina - Australia: 3 - 1");
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogCritical("Exception trying to add strings to List in getTeamDataList() ", ex.Message, ex.InnerException);
+            }
+           
+
+            return currentDataList;
+
+        }
+    }
+}
