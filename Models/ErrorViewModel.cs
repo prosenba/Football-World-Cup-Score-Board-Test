3f6c50613d6de101612d585fdc5722642e0b97be
Phillip Rosenbaum <Phillip@PhillipRosenbaum.com>
2023-01-08 18:36:54 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Football_World_Cup_Score_Board_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
